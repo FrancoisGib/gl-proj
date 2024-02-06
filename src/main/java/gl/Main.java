fa7f1c93c7bb89ff2d34c420b8db21bf78ae3c64
Gibier Francois <francois.gibier.etu@univ-lille.fr>
2024-02-06 16:03:08 +0100

    made it work

--- src/main/java/gl/Main.java
+++ src/main/java/gl/Main.java
@@ -1,9 +1,12 @@
 package gl;
 
+import org.apache.log4j.BasicConfigurator;
+
 import us.codecraft.webmagic.Spider;
 
 public class Main {
     public static void main(String[] args) {
+        BasicConfigurator.configure();  
         Spider.create(new GithubRepoPageProcessor()).addUrl("https://github.com/code4craft").thread(5).run();
     }
 }
